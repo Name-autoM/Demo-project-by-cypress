2400bae8aaab309e250b42ebb03bda2cdd4128bd
Patiphan meemung <85619171+Name-autoM@users.noreply.github.com>
2024-03-27 22:32:42 +0700

    Add files via upload

---
+++ e2e/examples/Demotest_for_element.js
@@ -0,0 +1,59 @@
+/// <reference types="Cypress" />
+ 
+describe('My Second Test Suite', function() 
+{
+
+it('Test Case for Checkbox',function() {
+
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
+cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+cy.get('input[type="checkbox"]').check(['option2','option3'])
+ 
+
+})
+
+it('Test Case for Static and Dynamic Dropdown',function() {
+//Static Dropdown
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+cy.get('select').select('option2').should('have.value','option2')
+ 
+//Dynamic dropdowns
+cy.get('#autocomplete').type('Ja')
+cy.get('.ui-menu-item div').each(($e1, index, $list) => {
+ 
+    if($e1.text()==="Japan")
+    {
+        cy.wrap($e1).click()
+    }
+ 
+})
+//autocomplete
+cy.get('#autocomplete').should('have.value','Japan')
+
+
+})
+it('Test Case for click button hide and show',function() {
+
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+//visible invisible
+cy.get('#displayed-text').should('be.visible')
+cy.get('#hide-textbox').click()
+cy.get('#displayed-text').should('not.be.visible')
+cy.get('#show-textbox').click()
+cy.get('#displayed-text').should('be.visible')
+ 
+
+})
+
+it('Test Case for check radio button',function() {
+
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+//radio buttons
+cy.get('[value="radio2"]').check().should('be.checked')
+ 
+ 
+  
+})
+
+})
